f88998df57dc21ce5462c8f4428d53e7753ac30f
Jinghan Yuan <jmy5395@psu.edu>
2024-10-14 12:24:31 -0400

    updated jbod numbers

--- jbod.h
+++ jbod.h
@@ -3,10 +3,10 @@
 #include <stdint.h>
 
 // Fill the below values with right values
-#define JBOD_NUM_DISKS            1
-#define JBOD_DISK_SIZE            1
-#define JBOD_BLOCK_SIZE           1
-#define JBOD_NUM_BLOCKS_PER_DISK  1
+#define JBOD_NUM_DISKS            16
+#define JBOD_DISK_SIZE            65536 // Each disk is 65536 bytes (256 blocks * 256 bytes per block)
+#define JBOD_BLOCK_SIZE           256
+#define JBOD_NUM_BLOCKS_PER_DISK  256  //65536/256
 
 typedef enum {
   JBOD_MOUNT,
